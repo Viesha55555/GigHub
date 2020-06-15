084368143240799268898b70601c5956234883ff
Viesturs Celms <viesturs.celms@knowledgeprice.com>
2020-06-15 16:01:54 +0300

    conventions edited

---
+++ Migrations/202006151251436_OvverideConventionforGigsOnceMore1.cs
@@ -0,0 +1,18 @@
+﻿namespace GigHub2.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class OvverideConventionforGigsOnceMore1 : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Gigs", "Venue", c => c.String(nullable: false, maxLength: 255));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Gigs", "Venue", c => c.String(nullable: false));
+        }
+    }
+}
